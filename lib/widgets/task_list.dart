0d811d6444eee471244885440dbf6f512673dc06
Macdeo <akinbiyi.adeola@gmail.com>
2023-07-11 01:12:56 +0200

    modified:   lib/widgets/task_list.dart 	modified:   lib/widgets/task_tile.dart

--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:todoey/models/Tasks.dart';
 
 import 'task_tile.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [];
   @override
   Widget build(BuildContext context) {
     return ListView(
--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -2,15 +2,46 @@
 
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  void checkboxCallBack(bool? checkboxState) {
+    setState(() {
+      isChecked = checkboxState ?? true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('Buy milk'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: null,
-      ),
+        title: Text(
+          'Buy milk',
+          style: TextStyle(
+              decoration: isChecked ? TextDecoration.lineThrough : null),
+        ),
+        trailing: TaskCheckbox(
+          checkboxState: isChecked,
+          toggleCheckboxState: checkboxCallBack,
+        ));
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool? checkboxState;
+  final ValueChanged<bool?>? toggleCheckboxState;
+
+  TaskCheckbox({this.checkboxState, this.toggleCheckboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
